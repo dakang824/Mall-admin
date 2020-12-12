632a4c2459165c6da96da402554994649a1366b8
e <18790336164@163.com>
2020-12-12 20:57:57 +0800

    开发中 修改首页统计数据

--- src/api/index.js
+++ src/api/index.js
@@ -2,22 +2,22 @@
  * @Author: yukang 1172248038@qq.com
  * @Description:
  * @Date: 2020-12-12 17:16:24
- * @LastEditTime: 2020-12-12 17:17:09
+ * @LastEditTime: 2020-12-12 17:21:57
  */
 import request from "@/utils/request";
 
-export function statOrderByDay(params) {
+export function statOrderByDay(data) {
   return request({
     url: "service/plat/admin/statOrderByDay",
-    method: "get",
-    params,
+    method: "post",
+    data,
   });
 }
 
-export function statOrderByMonth(params) {
+export function statOrderByMonth(data) {
   return request({
     url: "service/plat/admin/statOrderByMonth",
-    method: "get",
-    params,
+    method: "post",
+    data,
   });
 }
--- src/config/settings.js
+++ src/config/settings.js
@@ -2,7 +2,7 @@
  * @Author: yukang 1172248038@qq.com
  * @Description:
  * @Date: 2020-09-13 15:34:56
- * @LastEditTime: 2020-11-22 18:19:41
+ * @LastEditTime: 2020-12-12 17:54:28
  */
 /**
  * @copyright chuzhixin 1204505056@qq.com
